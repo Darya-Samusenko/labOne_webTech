791d7210cd915ec5f17b589b0eb064db08b7406b
Samusenko_D <loneluna@mail.ru>
2023-11-02 19:57:37 +0300

    remove trashcode from task 5

--- src/com/company/first_lab/Classes/sequence_remove_elements.java
+++ src/com/company/first_lab/Classes/sequence_remove_elements.java
@@ -1,11 +1,9 @@
 package com.company.first_lab.Classes;
 
 public class sequence_remove_elements {
-    private final int MIN = -2147483648;
-    private final int MAX = +2147483647;
     private boolean is_sorted_properly(int[] sequence){
         boolean is_sort=true;
-        for (int i = 0; (i <sequence.length-1)&&(is_sort) ; i++) {
+        for (int i = 0; (is_sort)&&(i <sequence.length-1); i++) {
             if(sequence[i]>sequence[i+1]){
                 is_sort=false;
             }
@@ -13,27 +11,10 @@ public class sequence_remove_elements {
         return is_sort;
     }
 
-    private int binary_search(int[] arr, int l, int r, int sElem){
-        int m = -1;
-        if (sElem < arr[l])
-            return l;
-        if (sElem > arr[r])
-            return r;
-        while (l <= r) {
-            m = (l + r) / 2;
-            if (sElem >= arr[m] && sElem < arr[m + 1])
-                return m + 1;
-            if (sElem < arr[m])
-                r = m - 1;
-            if (sElem > arr[m])
-                l = m + 1;
-        }
-        return m;
-    }
     public int find_quantity_of_elements_to_remove(int[] sequence){
         int k_num = 0;
         if(!is_sorted_properly(sequence)) {
-            boolean cont_search = false;
+            boolean cont_search;
             int[] flags_arr = new int[sequence.length];
             int step_element = 0;
             do{
